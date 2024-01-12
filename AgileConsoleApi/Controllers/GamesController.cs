0143bba68fabf62fa4725e61367aa386b33a6f71
ramontgomery <ryan.andrew.montgomery@gmail.com>
2024-01-11 21:02:12 -0500

    GamesController

--- AgileConsoleApi/Controllers/GamesController.cs
+++ AgileConsoleApi/Controllers/GamesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameFinder.Models.Models;
 using GameFinder.Services.GamesServices;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,23 @@ namespace AgileConsoleApi.Controllers
         public GamesController(IGamesRepo gamesRepo)
         {
             _gamesRepo = gamesRepo;
+        }
+        [HttpGet]
+        public async Task<IGamesRepo> GetGamesById([FromRoute] int gameId)
+        {
+            gameDetail? details = await _gamesRepo.GetNoteByIdAsync(gameId);
+
+
+            return details is not null
+                ? Ok(details)
+                : NotFound();
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IGamesRepo<GetGames>), 200)]
+        public async Task<IGamesRepo> GetAllGames()
+        {
+            var games = await _gamesRepo.GetAllGamesAsync();
+            return Ok(games);
         }
     }
 }
