b6f493543036992dda2dedbbd979a7a3614ee7e0
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2024-01-16 17:47:33 -0500

    commit

--- GameFinder.Services/GamesServices/GamesRepo.cs
+++ GameFinder.Services/GamesServices/GamesRepo.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameFinder.Data;
+using GameFinder.Models.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+namespace GameFinder.Services.GamesServices;
 
-namespace GameFinder.Services.GamesServices
+public class GamesRepo : IGamesRepo
 {
-    public class GamesRepo
+
+    private readonly AppDbContext _context;
+    public GamesRepo(AppDbContext context)
     {
-        
+        _context = context;
+    }
+
+    public Task<bool> AddGameToDbAsync(GameModel model)
+    {
+        if(model != null)
+        {
+            System.Console.WriteLine("This game is already in the system!");
+        }
+        else
+        {
+            
+        }
+        return ;
+    }
+
+    public async Task<GameModel?> GetGameModelByName(string GameName)
+    {
+        var gameEntity = await _context.Games.FindAsync(GameName);
+        if(gameEntity is null)
+        return null;
+
+        GameModel gameDetails = new()
+        {
+            GameName = gameEntity.GameName,
+            GameConsole = gameEntity.GameConsole,
+            Genre = gameEntity.Genre,
+            DateReleased = gameEntity.DateReleased,
+        };
+        return gameDetails;
     }
 }
--- GameFinder.Services/GamesServices/IGamesRepo.cs
+++ GameFinder.Services/GamesServices/IGamesRepo.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameFinder.Models.Models;
 
 namespace GameFinder.Services.GamesServices
 {
     public interface IGamesRepo
     {
-        
+        Task<bool> AddGameToDbAsync (GameModel model);
+        Task<GameModel?> GetGameModelByName(string GameName);
     }
 }
